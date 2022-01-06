1c15280b35d77d72bf75e1e3a52a07e4b0ffc5e7
KhaledAlabssi <khaled.absi5@gmail.com>
2022-01-06 18:25:00 +0100

    create app and handle error

---
+++ index.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv'
+import expressAsyncError from 'express-async-errors'
+import express from 'express'
+
+dotenv.config()
+const app = express()
+
+// error handler
+import notFoundMiddlewere from './middleware/notFound.js'
+import errorHandlerMiddlewere from './middleware/errorHandler.js'
+
+app.use(express.json())
+
+//routes:
+
+app.get('/', (req, res) => {
+    res.send('<h1>Email Project<h1> <a href="/send">Send Email</a>')
+
+})
+
+app.use(notFoundMiddlewere)
+app.use(errorHandlerMiddlewere)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        app.listen(port, () => console.log(`Server is listening on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
